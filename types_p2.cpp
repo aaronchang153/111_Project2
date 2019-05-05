e7b85b9b44b91080b1324e5ffd82c6aa5d64b41f
Aaron Chang <aaronchang153@gmail.com>
2019-05-04 21:22:08 -0700

    Various changes to make printing statuses easier

--- types_p2.cpp
+++ types_p2.cpp
@@ -1,9 +1,12 @@
 #include "types_p2.h"
+#include "p2_threads.h"
 #include "utils.h"
 
 
-
-void Person::set_gender(int data) { gender = data; }
+void Person::set_gender(int data) {
+	gender = data;
+	str_gender = (data == MALE) ? "Man" : "Woman";
+}
 int Person::get_gender(void)      { return gender; }
 
 void Person::set_order(unsigned long data) { order = data; }
@@ -13,6 +16,7 @@ void Person::set_use_order(unsigned long data) { use_order = data; }
 unsigned long Person::get_use_order(void)      { return use_order; }
 
 void Person::set_time(long data) { time_to_stay_ms = data; }
+long Person::get_time() { return time_to_stay_ms; }
 int Person::ready_to_leave(void) {
 	struct timeval t_curr;
 	gettimeofday(&t_curr, NULL);
@@ -26,15 +30,10 @@ int Person::ready_to_leave(void) {
 
 void Person::start(void) {
 	gettimeofday(&t_start, NULL);
-	printf("(%lu)th person enters the fittingroom: \n", order);
-	printf(" - (%lu) milliseconds after the creation\n", get_elasped_time(t_create, t_start));
 }
 
 void Person::complete(void) {
 	gettimeofday(&t_end, NULL);
-	printf("(%lu)th person comes out of the fittingroom: \n", order);
-	printf(" - (%lu) milliseconds after the creation\n", get_elasped_time(t_create, t_end));
-	printf(" - (%lu) milliseconds after using the fittingroom\n", get_elasped_time(t_start, t_end));
 }
 
 Person::Person() {
@@ -59,6 +58,7 @@ Fittingroom::~Fittingroom(){
 
 void Fittingroom::add_to_array(Person &p){
 	if(stalls != NULL && occupied != NULL && status != FULL){
+		// linear search for an open stall
 		for(int i = 0; i < num_stalls; i++){
 			if(!occupied[i]){
 				stalls[i] = p;
@@ -93,32 +93,67 @@ void Fittingroom::set_num_stalls(int n){
 	}
 }
 
-// You need to use this function to print the Fittingroom's status
-void Fittingroom::print_status(void) {
-	printf("Print fittingroom status\n");
-}
-
 int Fittingroom::get_status(void){
 	return status;
+}
+
+std::string Fittingroom::get_status_string(void){
+	switch(status){
+	case EMPTY:
+		return "EMPTY";
+	case MANPRESENT:
+		return "MENPRESENT";
+	case WOMANPRESENT:
+		return "WOMENPRESENT";
+	case FULL:
+		return "FULL";
+	default:
+		return "UNDEFINED";
+	};
+}
+
+int Fittingroom::get_occupied_count(void){
+	return occupied_count;
+}
+
+int Fittingroom::get_men_present(void){
+	return gender_flag ? 0 : occupied_count;
+}
+
+int Fittingroom::get_women_present(void){
+	return gender_flag ? occupied_count : 0;
 }
 
 void Fittingroom::add_person(Person &p) {
 	if(status != FULL){
 		if(status == EMPTY){
+			p.start();
 			add_to_array(p);
-			status = (p.get_gender() == MALE) ? MANPRESENT : WOMANPRESENT;
+			if(p.get_gender() == MALE){
+				status = MANPRESENT;
+				gender_flag = 0;
+			}
+			else{
+				status = WOMANPRESENT;
+				gender_flag = 1;
+			}
 		}
 		// if the person is the same gender as everyone else in the stalls
 		else if((status == MANPRESENT && p.get_gender() == MALE) ||
 			(status == WOMANPRESENT && p.get_gender() == FEMALE)){
+			p.start();
 			add_to_array(p);
 		}
 
 		// check if that addition made all the stalls full
 		if(occupied_count == num_stalls){
-			add_to_array(p);
 			status = FULL;
 		}
+
+		if(p.get_gender() == MALE)
+			man_enters(p);
+		else
+			woman_enters(p);
 	}
 }
 
@@ -128,18 +163,22 @@ int Fittingroom::remove_ready(void){
 	// check every stall to see if the person is ready to leave and remove if they are
 	for(int i = 0; i < num_stalls; i++){
 		if(occupied[i] && stalls[i].ready_to_leave()){
+			stalls[i].complete();
 			remove_from_array(i);
+			if(occupied_count == 0)
+				status = EMPTY;
+			else if(status == FULL) // if the stalls used to all be full
+				status = (stalls[i].get_gender() == MALE) ? MANPRESENT : WOMANPRESENT;
+
+			if(stalls[i].get_gender() == MALE)
+				man_leaves(stalls[i]);
+			else
+				woman_leaves(stalls[i]);
+
 			result = 1;
 		}
 	}
 
-	if(occupied_count == 0)
-		status = EMPTY;
-	else if(result == 1 && status == FULL){ // if anyone was removed and the stalls used to all be full
-		for(int i = 0; i < num_stalls; i++){
-			if(occupied[i]){
-				status = (stalls[i].get_gender() == MALE) ? MANPRESENT : WOMANPRESENT;
-			}
-		}
-	}
+
+	return result;
 }
