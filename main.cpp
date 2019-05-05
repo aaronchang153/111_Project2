49f4f2e27af7a4373dc716ab8b24bf8c021c8cd5
Aaron Chang <aaronchang153@gmail.com>
2019-05-04 21:20:06 -0700

    Seed random and removed commented code

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	srand((unsigned) time(NULL));
+
 	// This is to set the global start time
 	gettimeofday(&t_global_start, NULL);
 
@@ -44,50 +46,6 @@ int main(int argc, char** argv)
 
 	for(int i = 0; i < 3; i++)
 		pthread_join(tid[i], NULL);
-
-/*
-
-	pthread_t       tid = 0;
-	int             status = 0;
-	int             work = 0;
-
-
-	
-	// Example code for sleep and class usage
-	Person p1;
-	p1.set_order(1);
-
-	usleep(MSEC(200));
-	p1.start();
-
-
-	usleep(MSEC(150));
-	p1.complete();
-	///////////////////////////////////////////
-
-
-	if(pthread_create(&tid, NULL, threadfunc, NULL)) {
-		fprintf(stderr, "Error creating thread\n");		
-	}
-	usleep(MSEC(10));
-
-	for (int i=0; i<5; i++) {
-		printf("Wake up thread after (%d) seconds\n", (5-i));
-		usleep(MSEC(1000));
-	}
-
-	printf("Wake up thread\n");
-	status = pthread_cond_signal(&cond);
-	
-
-
-*/
-	/* wait for the second thread to finish */
-/*
-	if(pthread_join(tid, NULL)) {
-		fprintf(stderr, "Error joining thread\n");	
-	}
-*/
 
 	return 0;
 
